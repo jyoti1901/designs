e7fbd9c642f66d57e3f65a09393f9d2a4ed729e8
jyoti1901 <jyotivanera@gmail.com>
2023-03-20 22:20:01 +0530

    first commit

---
+++ design9/script.js
@@ -0,0 +1,21 @@
+const navBtn = document.getElementById('nav-btn');
+const CancelBtn = document.getElementById('cancel-btn');
+const sideNav = document.getElementById('sidenav');
+const modal = document.getElementById('modal');
+
+navBtn.addEventListener("click", function(){
+    sideNav.classList.add('show');
+    modal.classList.add('showModal');
+});
+CancelBtn.addEventListener('click', function(){
+    sideNav.classList.remove('show');
+    modal.classList.remove('showModal');
+});
+
+window.addEventListener('click', function(event){
+    if(event.target === modal){
+        sideNav.classList.remove('show');
+        modal.classList.remove('showModal');
+    }
+});
+
